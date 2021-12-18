4c1d741e7e30191673991b04c082416fb19dbf8b
PrettyMeng <zhoumeng9904@sjtu.edu.cn>
2021-12-18 21:07:15 +0800

    lab11 first 2 parts

--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -12,11 +12,30 @@ public class MazeCycles extends MazeExplorer {
 
     public MazeCycles(Maze m) {
         super(m);
+        distTo[0] = 0;
+        edgeTo[0] = 0;
+    }
+
+    private void dfs(int v) {
+        marked[v] = true;
+        announce();
+
+        for (int w : maze.adj(v)) {
+            if (!marked[w]) {
+                edgeTo[w] = v;
+                announce();
+                distTo[w] = distTo[v] + 1;
+                dfs(w);
+            } else if (edgeTo[v] != w) {
+                return;
+            }
+        }
     }
 
     @Override
     public void solve() {
         // TODO: Your code here!
+        dfs(0);
     }
 
     // Helper methods go here
